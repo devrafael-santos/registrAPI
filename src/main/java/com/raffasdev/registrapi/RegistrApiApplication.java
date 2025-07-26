7cfae8c38bda6db7256c3bdcb9a2166035330a98
devrafael-santos <raafael.cs@gmail.com>
2025-07-26 10:31:49 -0300

    first commit

---
+++ src/main/java/com/raffasdev/registrapi/RegistrApiApplication.java
@@ -0,0 +1,13 @@
+package com.raffasdev.registrapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrApiApplication.class, args);
+    }
+
+}
