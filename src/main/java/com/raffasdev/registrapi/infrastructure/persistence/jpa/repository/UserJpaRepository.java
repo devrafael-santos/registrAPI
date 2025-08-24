314aefcbae14de3a36cbb68e74b9561e84ab2594
devrafael-santos <raafael.cs@gmail.com>
2025-08-24 18:22:13 -0300

    Created CustomUserDetails wrapper for user entity

--- src/main/java/com/raffasdev/registrapi/infrastructure/persistence/jpa/repository/UserJpaRepository.java
+++ src/main/java/com/raffasdev/registrapi/infrastructure/persistence/jpa/repository/UserJpaRepository.java
@@ -3,10 +3,13 @@ package com.raffasdev.registrapi.infrastructure.persistence.jpa.repository;
 import com.raffasdev.registrapi.infrastructure.persistence.jpa.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserJpaRepository extends JpaRepository<UserEntity, UUID> {
     boolean existsByEmail(String email);
 
     boolean existsByUsername(String username);
+
+    Optional<UserEntity> findByEmail(String email);
 }
---
+++ src/main/java/com/raffasdev/registrapi/infrastructure/security/CustomUserDetails.java
@@ -0,0 +1,30 @@
+package com.raffasdev.registrapi.infrastructure.security;
+
+import com.raffasdev.registrapi.infrastructure.persistence.jpa.entity.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final UserEntity userEntity;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return userEntity.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return userEntity.getEmail();
+    }
+}
